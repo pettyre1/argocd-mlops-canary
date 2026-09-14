a867a7ea5df1f61117a4c476b85315501d8850d7
rpetty <ryan.e.petty@gmail.com>
2026-09-14 09:52:40 -0400

    induce latency

--- app/nlp.py
+++ app/nlp.py
@@ -8,7 +8,7 @@ def extract_named_entities(text: str):
     doc = nlp(text)
 
     # Simulate latency
-    #time.sleep(3)
+    time.sleep(3)
     
     # Format entities as a list of dictionaries for clean JSON responses
     return [{"text": ent.text, "label": ent.label_} for ent in doc.ents]
